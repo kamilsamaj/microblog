b2beb5737e1f0432e0566e8e4b9a53d0ad9c27b7
Kamil Samaj <kamil.samaj@accolade.com>
2019-02-11 21:51:05 +0100

    avatar working

--- app/models.py
+++ app/models.py
@@ -11,6 +11,8 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(128), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -24,7 +26,6 @@ class User(UserMixin, db.Model):
     def avatar(self, size=128):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
         return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
-
 
 
 class Post(db.Model):
